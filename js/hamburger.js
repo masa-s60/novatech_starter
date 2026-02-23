63add3da3ad301c574736dd94f4b4d3fbe54f59e
草野昌俊 <kusanomasatoshi@kusanomasatoshinoMacBook-Air.local>
2026-02-23 12:53:15 +0900

    header complete

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,18 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburgerMenu = document.querySelector('.c-hamburger');
-  const nav = document.querySelector('.c-header__nav');
-  const overlay = document.querySelector('.c-overlay');
+  const nav = document.querySelector('.c-drawer');
   
   const toggleMenu = () => {
     const isOpen = hamburgerMenu.classList.toggle("is-open");
     nav.classList.toggle("is-open", isOpen);
-    overlay.classList.toggle("is-open", isOpen);
 
     hamburgerMenu.setAttribute("aria-expanded", isOpen ? "true" : "false");
     nav.setAttribute("aria-hidden", isOpen ? "false" : "true");
-    overlay.setAttribute("aria-hidden", isOpen ? "false" : "true");
   }
 
   hamburgerMenu.addEventListener('click', toggleMenu);
-  overlay.addEventListener("click", toggleMenu);
+  nav.addEventListener("click", toggleMenu);
 });
